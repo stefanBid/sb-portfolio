ba1295794fc1aa29b70ca6ed696e89831bb06d75
stefanBid <biddau.stefano99@gmail.com>
2023-10-22 16:14:51 +0200

    fix(commitLint): change yml file

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,13 +1,12 @@
 module.exports = {
   parserPreset: {
     parserOpts: {
-      headerPattern:
-        /^((fix|feat)\(([^)]+)\): (.+)|(Merge pull request #[0-9]+ from .+))$/,
+      headerPattern: /^(fix|feat)(\(([^)]*)\))?: (.+)$/,
       headerCorrespondence: ['type', 'scope', 'subject'],
     },
   },
   rules: {
-    'type-enum': [2, 'always', ['fix', 'feat', 'Merge pull request']],
+    'type-enum': [2, 'always', ['fix', 'feat']],
     'scope-empty': [1, 'never'],
     'subject-empty': [2, 'never'],
   },
