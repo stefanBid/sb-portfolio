03b70994902e3f298bbcb6d8052225c92da657b5
stefanBid <biddau.stefano99@gmail.com>
2023-11-19 17:08:35 +0100

    fix (HomePageView): bugs resolutions

--- src/components/global/Button/BaseButton.vue
+++ src/components/global/Button/BaseButton.vue
@@ -3,10 +3,10 @@
   import { IconName } from '@/types/icon.type';
   import { computed, useSlots } from 'vue';
 
-  type ButtonKind = 'Primary' | 'Secondary';
+  type ButtonVariant = 'Primary' | 'Secondary';
 
   export interface BaseButtonProps {
-    kind?: ButtonKind;
+    variant?: ButtonVariant;
     type?: 'solid' | 'outline';
     size?: 'square' | 'rectangle';
     icon?: IconName;
@@ -14,7 +14,7 @@
   }
 
   const props = withDefaults(defineProps<BaseButtonProps>(), {
-    kind: 'Primary',
+    variant: 'Primary',
     type: 'solid',
     size: 'rectangle',
     icon: undefined,
@@ -27,32 +27,18 @@
 
   const buttonKindMap = {
     Primary: {
-      solid: {
-        class: 'bg-sb-sky-blue-100 text-black dark:text-white shadow-none',
-        hover:
-          'hover:bg-sb-sky-blue-200 hover:shadow-button hover:shadow-sb-sky-blue-200 ',
-        active: 'active:bg-sb-sky-blue-300 active:shadow-sb-sky-blue-300',
-      },
-      outline: {
-        class:
-          'border border-2 border-sb-sky-blue-100 bg-transparent text-sb-sky-blue-100  shadow-none',
-        hover:
-          'hover:bg-sb-sky-blue-100 hover:text-black dark:hover:text-white hover:shadow-button hover:shadow-sb-sky-blue-100',
-        active:
-          'active:bg-sb-sky-blue-100 active:text-black dark:active:text-white active:shadow-button active:shadow-sb-sky-blue-100',
-      },
+      solid: `
+        border-2 border-sb-sky-blue-100 bg-sb-sky-blue-100 text-black dark:text-white shadow-none
+        hover:bg-sb-sky-blue-200 hover:shadow-button hover:shadow-sb-sky-blue-200
+        active:bg-sb-sky-blue-300 active:shadow-sb-sky-blue-300`,
+      outline: `
+        border-2 border-sb-sky-blue-100 bg-transparent text-sb-sky-blue-100 shadow-none
+        hover:bg-sb-sky-blue-200 hover:text-black dark:hover:text-white hover:shadow-button hover:shadow-sb-sky-blue-100
+        active:bg-sb-sky-blue-300 active:text-black dark:active:text-white active:shadow-button active:shadow-sb-sky-blue-100`,
     },
     Secondary: {
-      solid: {
-        class: '',
-        hover: '',
-        active: '',
-      },
-      outline: {
-        class: '',
-        hover: '',
-        active: '',
-      },
+      solid: '',
+      outline: '',
     },
   };
 
@@ -65,12 +51,7 @@
   const buttonDynamicCss = computed(() => {
     let cssClass: string[] = [];
 
-    cssClass.push(buttonKindMap[props.kind][props.type].class);
-
-    if (!props.disabled) {
-      cssClass.push(buttonKindMap[props.kind][props.type].hover);
-      cssClass.push(buttonKindMap[props.kind][props.type].active);
-    }
+    cssClass.push(buttonKindMap[props.variant][props.type]);
 
     props.size === 'rectangle'
       ? cssClass.push('px-4 py-2')
@@ -87,7 +68,7 @@
   <button
     v-bind="$attrs"
     :disabled="props.disabled"
-    class="font-semibold transition-all duration-300 ease-in-out rounded-xl group disabled:opacity-40 disabled:cursor-not-allowed hover:scale-110"
+    class="font-semibold transition-all duration-300 ease-in-out rounded-xl group disabled:pointer-events-none disabled:opacity-40 hover:scale-110"
     :class="[buttonDynamicCss, $attrs.class]"
   >
     <span class="text-xl">
@@ -96,7 +77,6 @@
     <BaseIcon
       v-if="props.icon"
       :icon="props.icon"
-      class="transition-transform duration-300 ease-in-out"
       :class="countSlotElements() !== 1 ? 'text-2xl' : 'text-4xl'"
     />
   </button>
--- src/components/global/Title/BaseTitle.vue
+++ src/components/global/Title/BaseTitle.vue
@@ -1,48 +1,35 @@
 <script setup lang="ts">
-  import { useSlots, computed, useAttrs } from 'vue';
+  import { useAttrs } from 'vue';
 
   const attrs = useAttrs();
-  const slots = useSlots();
 
   export interface BaseTitleProps {
+    text: string;
+    prettyText?: string;
     size?: 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
-    type?: 'simple' | 'pretty' | 'forLink';
-    linkTo?: Object;
   }
 
   const props = withDefaults(defineProps<BaseTitleProps>(), {
+    prettyText: undefined,
     size: 'md',
-    type: 'simple',
-  });
-
-  const areMoreSlots = computed(() => {
-    const defaultSlot = slots.default?.().length || 0;
-    const prettySlot = slots.pretty?.().length || 0;
-    return defaultSlot + prettySlot > 1 ? true : false;
   });
 </script>
 
 <template>
-  <component
-    :is="props.type === 'forLink' ? 'router-link' : 'div'"
-    :to="props.type === 'forLink' ? props.linkTo : undefined"
-    class="inline-flex items-center font-semibold leading-normal w-fit group"
+  <div
+    class="inline-flex items-center font-semibold leading-normal transition-all duration-300 ease-in-out gap-x-3 w-fit group"
     :class="[
       attrs.class,
-      { 'transition-all duration-300 ease-in-out': props.type !== 'forLink' },
       { 'text-5xl': props.size === 'xxl', 'text-3xl': props.size === 'xl' },
-      { 'gap-x-3': areMoreSlots },
-      { 'hover:cursor-pointer ': props.type === 'forLink' },
     ]"
   >
-    <slot name="default" />
+    <span> {{ props.text }} </span>
 
     <span
-      v-if="props.type === 'pretty'"
+      v-if="props.prettyText"
       class="inline-flex items-center text-sb-sky-blue-100"
-      :class="[{ 'gap-x-3': areMoreSlots }]"
     >
-      <slot name="pretty" />
+      {{ props.prettyText }}
     </span>
-  </component>
+  </div>
 </template>
